9d9a9b2d661cd3aa34e278b8af6961dc5feede88
UN64DD <azhar.janius2005@gmail.com>
2026-08-23 20:46:00 +0800

    scene-fix

--- src/scene.js
+++ src/scene.js
@@ -6,7 +6,8 @@ export function createScene() {
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x777777);
 
-    const camera = new THREE.PerspectiveLiveCamera(75, gameWindow.offsetWidth / gameWindow.offsetHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, gameWindow.offsetWidth / gameWindow.offsetHeight, 0.1, 1000);
+    camera.position.z = 5;
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
